37f5468dd16dc1ab76d97cf88f6114aaf9110a2e
Ziroo <yusufholat@gmail.com>
2023-09-13 01:57:54 +0300

    create IEntityRepository

---
+++ BlogSite.Shared/Data/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using BlogSite.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+
+namespace BlogSite.Shared.Data.Abstract
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        Task<T> GetAsync(Expression<Func<T,bool>> predicate, params Expression<Func<T, object>>[] includeProperties); //multi include
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task <bool> AnyAsnyc(Expression<Func<T, bool>> predicate);
+        Task <int> CountAsnyc(Expression<Func<T, bool>> predicate);
+
+    }
+}
