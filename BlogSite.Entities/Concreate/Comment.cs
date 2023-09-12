50dc3476cb4b2ab0829cb4ed2b8baf174d031965
Ziroo <yusufholat@gmail.com>
2023-09-13 01:54:41 +0300

    added data classes

---
+++ BlogSite.Entities/Concreate/Comment.cs
@@ -0,0 +1,13 @@
+﻿using BlogSite.Shared.Entities.Abstract;
+
+
+namespace BlogSite.Entities.Concreate
+{
+    public class Comment : EntityBase, IEntity
+    {
+        public int ArticleId { get; set; }
+        public string Text { get; set; }
+        public Article Article { get; set; }
+
+    }
+}
---
+++ BlogSite.Entities/Concreate/User.cs
@@ -0,0 +1,20 @@
+﻿
+using BlogSite.Shared.Entities.Abstract;
+
+
+namespace BlogSite.Entities.Concreate
+{
+    public class User : EntityBase, IEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Picture { get; set; }
+        public string Description { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public string Username { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
