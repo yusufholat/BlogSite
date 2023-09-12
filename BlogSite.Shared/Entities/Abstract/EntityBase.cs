b66d262786ac7333e4c3c7295ea4d3a88fa56691
Ziroo <yusufholat@gmail.com>
2023-09-13 01:58:15 +0300

    code refactor

--- BlogSite.Shared/Entities/Abstract/EntityBase.cs
+++ BlogSite.Shared/Entities/Abstract/EntityBase.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace BlogSite.Shared.Entities.Abstract
 {
@@ -14,7 +10,8 @@ namespace BlogSite.Shared.Entities.Abstract
         public virtual bool IsDeleted { get; set; } = false;
         public virtual bool IsActive { get; set; } = true;
 
-        public virtual string CreatedByName { get; set; }
-        public virtual string ModifiedByName { get; set; }
+        public virtual string CreatedByName { get; set; } = "Admin";
+        public virtual string ModifiedByName { get; set; } = "Admin";
+        public virtual string Note { get; set; }
     }
 }
--- BlogSite.Shared/Entities/Abstract/IEntity.cs
+++ BlogSite.Shared/Entities/Abstract/IEntity.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace BlogSite.Shared.Entities.Abstract
 {
-    internal interface IEntity
+    public interface IEntity
     {
     }
 }
