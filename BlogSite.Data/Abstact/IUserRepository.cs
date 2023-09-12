ae424a16ee833b788ca3f5b3397fdd3faa1cf601
Ziroo <yusufholat@gmail.com>
2023-09-13 01:58:52 +0300

    connection with generic type repositories

---
+++ BlogSite.Data/Abstact/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using BlogSite.Entities.Concreate;
+using BlogSite.Shared.Data.Abstract;
+
+
+namespace BlogSite.Data.Abstact
+{
+    public interface IUserRepository : IEntityRepository<User>
+    {
+
+    }
+}
