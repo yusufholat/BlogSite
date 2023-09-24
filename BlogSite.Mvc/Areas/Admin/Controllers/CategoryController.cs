5ef4ea246a839f3beb8a00a3ac6e2aa66da0900a
Ziroo <yusufholat@gmail.com>
2023-09-25 01:32:17 +0300

    datatable works

---
+++ BlogSite.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BlogSite.Services.Abstract;
+using BlogSite.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogSite.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService) { 
+            _categoryService = categoryService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await _categoryService.GetAll(); 
+            if (result.ResultStatus == ResultStatus.Success) {
+                return View(result.Data);
+            }
+
+            return View();
+        }
+    }
+}
