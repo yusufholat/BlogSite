00c407ce411b53be146634f492cdd8c10cb02ed2
Ziroo <yusufholat@gmail.com>
2023-09-27 16:53:06 +0300

    added user classes

---
+++ BlogSite.Entities/Dtos/UserListDto.cs
@@ -0,0 +1,11 @@
+﻿using BlogSite.Entities.Concreate;
+using BlogSite.Shared.Entities.Abstract;
+
+
+namespace BlogSite.Entities.Dtos
+{
+    public class UserListDto : DtoGetBase
+    {
+        public IList<User>? Users { get; set; }
+    }
+}
---
+++ BlogSite.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using BlogSite.Entities.Concreate;
+using BlogSite.Entities.Dtos;
+using BlogSite.Shared.Utilities.Results.ComplexTypes;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogSite.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            return View(new UserListDto
+            {
+                Users = users,
+                ResultStatus = ResultStatus.Success
+            });
+        }
+    }
+}
