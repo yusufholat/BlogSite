868bcb9f9fd266d5215b7794499cd2bf97e48412
Ziroo <yusufholat@gmail.com>
2023-10-06 17:11:19 +0300

    admin menu viewcomponent

---
+++ BlogSite.Mvc/Areas/Admin/Models/UserWithRolesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BlogSite.Entities.Concreate;
+
+namespace BlogSite.Mvc.Areas.Admin.Models
+{
+    public class UserWithRolesViewModel
+    {
+        public User User { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
